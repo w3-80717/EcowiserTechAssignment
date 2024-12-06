5ba35d6ad7293c9f1d966543362124fce4e6e73b
Ashwini <111645320+w3-80717@users.noreply.github.com>
2024-12-06 12:49:10 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const cors = require('cors');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -8,8 +9,9 @@ const PORT = 3001;
 
 app.use(bodyParser.json());
 app.use(cors());
-
+app.use(express.static(__dirname+"/public"));
 app.use('/api', productRoutes);
+app.get('/', (req,res)=>res.sendFile(path.join(__dirname,"/public/index.html")));
 
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`);
